ffe86481063076689a61966c3f516b015979da23
Majid Jamaleldine <majid.jamaleldine@gmail.com>
2018-05-07 18:11:41 -0500

    got the query logic for purchasing a product down

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -22,15 +22,99 @@ connection.connect(function(err) {
    
     console.log('connected as id ' + connection.threadId);
 });
- 
-connection.query("SELECT * FROM products", function (err, res) {
-  if (err) throw err;
-  res.map(elem => {
-      console.log(`Product: ${elem.product_name}`);
-      console.log(`Price: $${elem.price}`);
-      console.log(`Department: ${elem.department_name}`);
-      console.log(`ID: ${elem.item_id}\n`);
-  })
-});
- 
-connection.end();
+
+function startShopping () {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        listProducts(res);
+        purchaseQuery(res);
+    });
+}
+
+function listProducts(dbProductArray) {
+    console.log("\nAll available products:\n")
+    dbProductArray.map(elem => {
+        if (elem.stock_quantity > 0) {
+          console.log(`Product: ${elem.product_name}`);
+          console.log(`Price: $${elem.price}`);
+          console.log(`ID: ${elem.item_id}\n`);
+          // console.log(`Department: ${elem.department_name}`);
+        }
+    })
+}
+
+function purchaseQuery(dbProductArray) {
+    inquirer.prompt(
+        {
+            type: "type",
+            message: "What product would you like to purchase? Give the pruduct ID number: ",
+            name: "productID"
+        }
+    ).then(results => {
+        if (!isNaN(results.productID) && results.productID > 0 && results.productID <= dbProductArray.length) {
+            purchaseQuantityQuery(dbProductArray[results.productID-1]);
+        } else {
+            console.log("Please try again with a viable ID number.");
+            purchaseQuery(dbProductArray);
+        }
+    })
+    .catch(err => {
+        if (err) throw err;
+    })
+}
+
+function purchaseQuantityQuery(dbProduct) {
+    inquirer.prompt(
+        {
+            type: "type",
+            message: "How many would you like to purchase?",
+            name: "productQuantity"
+        }
+    ).then(results => {
+        if (!isNaN(results.productQuantity) && results.productQuantity > 0 && results.productQuantity <= dbProduct.stock_quantity) {
+            console.log(`\nSure! Here you go.\nThat will cost $${results.productQuantity * dbProduct.price}.\nThanks for shopping!\n`);
+            continueShoppingQuery();
+        } else if (results.productQuantity > dbProduct.stock_quantity) {
+            console.log(`Sorry, we do not have that much stock. We only have ${dbProduct.stock_quantity}. Please try again.`);
+            purchaseQuantityQuery(dbProduct);
+        } else {
+            console.log("Please try again with a viable ID number.");
+            purchaseQuantityQuery(dbProduct);
+        }
+    })
+    .catch(err => {
+        if (err) throw err;
+    })
+}
+
+function continueShoppingQuery() {
+    inquirer.prompt(
+        {
+            type: "list",
+            message: "Would you like to keep shopping?",
+            choices: ["yes", "no"],
+            name: "continueShoppingAnswer"
+        }
+    ).then(results => {
+        switch (results.continueShoppingAnswer) {
+            case "yes":
+            startShopping();
+            break;
+            
+            case "no":
+            console.log("Thanks for shopping with us! Bye.");
+            connection.end();
+            break;
+
+            default:
+            console.log("Please try again.");
+            continueShoppingQuery();
+            break;
+        }
+    })
+    .catch(err => {
+        if (err) throw err;
+    })
+}
+
+startShopping();
